19f30d30f385f98d079ba8a692b8487e9463ba10
kassoum sanogo <kassoum.sanogo@outlook.com>
2024-10-31 15:26:08 +0100

    Update loup_garou_AI2.py

--- loup_garou_AI2.py
+++ loup_garou_AI2.py
@@ -9,7 +9,7 @@ from colorama import Fore, Style
 colorama.init()
 
 #Clé API à mettre, il faut recuperer les clés chez Mystral AI sur huggingface
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": "Bearer ************************"}
 current_model_number = 0
 total_model_number = 5
 
